159082a495503107ebced2d4a1e201096898e94a
Igor César de Oliveira <igorcesar.g.o@gmail.com>
2023-03-31 07:15:00 -0300

    011

--- exercicios/aula11/ex001.js
+++ exercicios/aula11/ex001.js
@@ -1,2 +1,8 @@
-console.log('o console funcionou corretamente')
 
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // condição simples!
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de seguraça')
