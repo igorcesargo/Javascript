66344682b2e11aca2e570e51eaf7ec55ba9aedcb
Igor César de Oliveira <igorcesar.g.o@gmail.com>
2023-04-12 07:55:32 -0300

    repetiçoes

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,25 @@
+
+var c = 1
+ do {
+    console.log(`Passo ${c}`)   
+    c++
+}while (c <= 8)
+
+
+/*var c = 1
+while (c <= 8) {
+    console.log(`Passo ${c}`)   
+    c++
+}
+*/
+
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+console.log('Passo 7')
+console.log('Passo 8')*/
