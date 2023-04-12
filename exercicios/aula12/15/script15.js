782b933b05dbb1c743a72e4fae625c499dfdb847
Igor César de Oliveira <igorcesar.g.o@gmail.com>
2023-04-12 06:33:27 -0300

    atualiza 15

--- exercicios/aula12/15/script15.js
+++ exercicios/aula12/15/script15.js
@@ -20,21 +20,29 @@ if(fano.value.length == 0 || Number(fano.value) > ano) {
             img.setAttribute('src', '0-3-menino.jpg')
         } else if (idade < 9) {
             //criança
+            img.setAttribute('src','4-9-menino.jpg')
         } else if (idade < 14) {
             //Pre-adolescente
+            img.setAttribute('src','10-14-menino.jpg')
         } else if (idade < 17) {
             //Adolescente
+            img.setAttribute('src','15-17-menino.jpg')
         } else if (idade < 24) {
             //Jovem
+            img.setAttribute('src','18-24-homem.jpg')
         } else if (idade < 34) {
             //Adulto
+            img.setAttribute('src','25-34-homem.jpg')
         } else if (idade < 44) {
             //pre
+            img.setAttribute('src','35-44-homem.jpg')
         } else if (idade < 54) {
             //Menopausa
+            img.setAttribute('src','45-55-homem.jpg')
         }
         else {
             //Idoso
+            img.setAttribute('src','56-homem.jpg')
         }
     }else if (fsex[1].checked){
         genero = 'Mulher'
@@ -43,25 +51,33 @@ if(fano.value.length == 0 || Number(fano.value) > ano) {
             img.setAttribute('src','0-3.jpg')
         } else if (idade < 9) {
             //criança
+            img.setAttribute('src','4-9-menina.jpg')
         } else if (idade < 14) {
             //Pre-adolescente
+            img.setAttribute('src','10-14-menina.jpg')
         } else if (idade < 17) {
             //Adolescente
+            img.setAttribute('src','15-17-menina.jpg')
         } else if (idade < 24) {
             //Jovem
+            img.setAttribute('src','18-24-mulher.jpg')
         } else if (idade < 34) {
             //Adulto
+            img.setAttribute('src','25-34-mulher.jpg')
         } else if (idade < 44) {
             //pre
+            img.setAttribute('src','35-44-mulher.jpg')
         } else if (idade < 54) {
             //Menopausa
+            img.setAttribute('src','45-55-mulher.jpg')
         }
         else {
             //Idoso
+            img.setAttribute('src','56-mulher.jpg')
         }
     }
     res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos. <br>`
     res.appendChild(img)
     
 }
