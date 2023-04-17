9dbf25c7caf073bc6337978c466bb5ce1a24f063
Igor César de Oliveira <igorcesar.g.o@gmail.com>
2023-04-17 08:06:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,61 @@ $(document).ready(function() {
     var produtos = $(".produtos");
     var setaEsquerda = $(".seta-esquerda");
     var setaDireita = $(".seta-direita");
-    
+  
     var passoRolagem = 320; // ajuste a quantidade de pixels a ser rolada de cada vez
-    
+  
+    // duplicar a lista de produtos para permitir rolagem infinita
+    produtos.append(produtos.html());
+  
     // habilitar a rolagem por gestos de deslize em dispositivos móveis
     produtosContainer.swipe({
       swipeLeft: function() {
-        produtos.animate({marginLeft: "-=" + passoRolagem + "px"}, 500);
+        produtos.animate({marginLeft: "-=" + passoRolagem + "px"}, 500, function() {
+          var margemEsquerda = parseInt(produtos.css("marginLeft"));
+          var larguraProdutos = produtos.width();
+          var larguraContainer = produtosContainer.width();
+          if (larguraProdutos - margemEsquerda < larguraContainer) {
+            produtos.css("marginLeft", "0px");
+          }
+        });
       },
       swipeRight: function() {
-        produtos.animate({marginLeft: "+=" + passoRolagem + "px"}, 500);
+        produtos.animate({marginLeft: "+=" + passoRolagem + "px"}, 500, function() {
+          var margemEsquerda = parseInt(produtos.css("marginLeft"));
+          if (margemEsquerda >= 0) {
+            produtos.css("marginLeft", "-" + (produtos.width() / 2) + "px");
+          }
+        });
       },
       allowPageScroll: "auto"
     });
-    
+  
     // habilitar a rolagem clicando nas setas de navegação
     setaEsquerda.click(function() {
-      produtos.animate({marginLeft: "+=" + passoRolagem + "px"}, 500);
+      produtos.animate({marginLeft: "+=" + passoRolagem + "px"}, 500, function() {
+        var margemEsquerda = parseInt(produtos.css("marginLeft"));
+        if (margemEsquerda >= 0) {
+          produtos.css("marginLeft", "-" + (produtos.width() / 2) + "px");
+        }
+      });
     });
-    
+  
     setaDireita.click(function() {
-      produtos.animate({marginLeft: "-=" + passoRolagem + "px"}, 500);
+      produtos.animate({marginLeft: "-=" + passoRolagem + "px"}, 500, function() {
+        var margemEsquerda = parseInt(produtos.css("marginLeft"));
+        var larguraProdutos = produtos.width();
+        var larguraContainer = produtosContainer.width();
+        if (larguraProdutos - margemEsquerda < larguraContainer) {
+          produtos.css("marginLeft", "0px");
+        }
+      });
     });
-    
+  
     // habilitar a rolagem arrastando com o mouse
     produtosContainer.mousedown(function(e) {
       var posicaoInicial = e.pageX;
       var posicaoAtual = posicaoInicial;
-      
+  
       $(document).mousemove(function(e) {
         posicaoAtual = e.pageX;
         var distancia = posicaoAtual - posicaoInicial;
@@ -38,27 +65,11 @@ $(document).ready(function() {
         posicaoInicial = posicaoAtual;
       });
     });
-    
+  
     $(document).mouseup(function() {
       $(document).off("mousemove");
     });
   });
-
-
-
-
-
-  jQuery(document).ready(function() {
-    // variáveis
-    var larguraCarousel = jQuery(".carousel").outerWidth();
-    var itemWidth = jQuery(".carousel-item").outerWidth(true);
-    var numItens = jQuery(".carousel-item").length;
-    var numItensVisiveis = Math.floor(larguraCarousel / itemWidth);
-    var posicaoAtual = 0;
-    var posicaoMaxima = numItens - numItensVisiveis;
-    
-    // atualiza o estado dos botões de controle
-    function atualizarControles() {
   
  /*
 
@@ -130,6 +141,4 @@ $(document).ready(function() {
         posicaoAtual = e.pageX;
         var distancia = posicaoAtual -
 
-
-
-
+*/
