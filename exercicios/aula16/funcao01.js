d331e20d674541bc0b9041ff58683c018c4e24b5
Igor César de Oliveira <igorcesar.g.o@gmail.com>
2023-04-19 07:56:02 -0300

    aula função

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2==0) {
+        return'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+/*let res = parimp(4)
+console.log(`Parimpar ${res}`)*/
+
+console.log(parimpar(2334))
